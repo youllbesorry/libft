f5262ea1b48dd38b7b070ad3ebad2c27f17f0d32
Benjamin Faure <bfaure@z1r6p5.42lyon.fr>
2022-11-24 17:28:38 +0100

    split working and pass unit test and war machine but not protected on unit test, add itoa and main_itoa not done

---
+++ Libft/ft_itoa.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfaure <bfaure@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 16:48:34 by bfaure            #+#    #+#             */
+/*   Updated: 2022/11/24 17:25:51 by bfaure           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	else if (nb < 0)
+	{
+		write(1, "-", 1);
+		ft_putnbr(nb *= -1);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_itoa(nb);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+
+	n += '0';
+	str = malloc(sizeof(char) * ft_strlen(n));
+}
--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -11,115 +11,84 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-static size_t	tab_len(char const *s, char c, int n, size_t i)
+static	size_t	ft_cont_word(char const *s, char c)
 {
-	size_t	j;
+	size_t	i;
+	size_t	n;
 
-	j = 0;
+	i = 0;
+	n = 0;
+	while (s[i])
+	{
+		i++;
+		if (s[i] == c && s[i - 1] != c)
+			n++;
+		if (s[i] == '\0' && s[i - 1] != c)
+			n++;
+	}
+	return (n + 1);
+}
+
+static	size_t	ft_len_word(char const	*s, char c, size_t j, size_t n)
+{
+	size_t	i;
+
+	i = 0;
 	if (n == 0)
 	{
-		while (s[i] != '\0')
-		{
-			if (s[i] == c)
-				j++;
-			i++;
-		}
-		return (i - j);
+		while (s[j] != c && s[j] != '\0')
+			j++;
+		return (j);
 	}
 	if (n == 1)
 	{
-		while (s[i] == c)
-			i++;
-		return (i);
+		while (s[j] == c)
+			j++;
+		return (j);
 	}
 	return (0);
 }
 
-static void	*ft_free(char **strs, size_t i)
+static	char	**ft_free(char **strs)
 {
-	size_t	j;
+	size_t	i;
 
-	j = 0;
-	while (j <= i)
+	i = 0;
+	while (strs[i])
 	{
-		free(strs[j]);
-		j++;
+		free(strs[i]);
+		i++;
 	}
 	free(strs);
 	return (NULL);
 }
 
-static char	**ft_append(char const *s, char c, char **strs, size_t len)
-{
-	size_t	i;
-	size_t	j;
-	size_t	n;
-
-	i = tab_len(s, c, 1, 0);
-	n = 0;
-	j = 0;
-	while (s[i] != '\0' && j <= len)
-	{
-		if (s[i] != c)
-			strs[j][n++] = s[i];
-		else
-		{
-			strs[j][n] = '\0';
-			j++;
-			n = 0;
-			i = tab_len(s, c, 1, i) - 1;
-		}
-		i++;
-	}
-	if (j <= 1)
-			j++;
-	strs[j] = NULL;
-	return (strs);
-}
-
-static char	**ft_alloc_2d_tab(char const *s, char c, char **strs, size_t len)
-{
-	size_t	i;
-	size_t	j;
-	size_t	n;
-
-	i = tab_len(s, c, 1, 0);
-	j = 0;
-	n = 0;
-	strs = malloc(sizeof(char *) * (len + 1));
-	if (!strs)
-		return (NULL);
-	while (i != ft_strlen(s) + 1)
-	{
-		if (s[i] != c && s[i] != '\0')
-			j++;
-		else if (s[i] == c || s[i] == '\0')
-		{
-			strs[n++] = malloc(sizeof(char) * (j + 1));
-			if (!strs[n - 1])
-				return (ft_free(strs, i));
-			j = 0;
-			i = tab_len(s, c, 1, i);
-		}
-		i++;
-	}
-	return (strs);
-}
-
 char	**ft_split(char const *s, char c)
 {
-	size_t	len;
 	char	**strs;
+	size_t	i;
+	size_t	nbword;
+	size_t	j;
+	size_t	k;
 
-	strs = NULL;
-	len = tab_len(s, c, 0, 0) + 1;
-	strs = ft_alloc_2d_tab(s, c, strs, len);
+	nbword = ft_cont_word(s, c);
+	strs = malloc((sizeof(char *)) * nbword);
 	if (!strs)
 		return (NULL);
-	if (c == '\0')
-		return (strs);
-	strs = ft_append(s, c, strs, len);
+	j = 0;
+	i = 0;
+	k = 0;
+	while (nbword > i)
+	{
+		k = ft_len_word(s, c, j, 1);
+		j = ft_len_word(s, c, k, 0) - k;
+		strs[i] = ft_substr(s, k, j);
+		if (strs[i] == NULL)
+			return (ft_free(strs));
+		i++;
+		j += k;
+	}
+	strs[i - 1] = NULL;
 	return (strs);
 }
