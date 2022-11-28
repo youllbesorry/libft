0df08211a117583b09181aaadf0695aa227112c8
root <root@Ben>
2022-11-28 11:01:03 +0100

    itoa

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,37 +6,76 @@
 /*   By: bfaure <bfaure@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 16:48:34 by bfaure            #+#    #+#             */
-/*   Updated: 2022/11/24 17:25:51 by bfaure           ###   ########lyon.fr   */
+/*   Updated: 2022/11/25 19:12:18 by bfaure           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr(int nb)
+static size_t	ft_intlen(long nb)
 {
-	if (nb == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		return ;
-	}
+	size_t	len;
+
+	len = 0;
+	if (nb == 0)
+		return (1);
 	else if (nb < 0)
+		len++;
+	while (nb != 0)
 	{
-		write(1, "-", 1);
-		ft_putnbr(nb *= -1);
+		nb /= 10;
+		len++;
 	}
-	else if (nb > 9)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else
-		ft_itoa(nb);
+	return (len);
 }
 
-char	*ft_itoa(int n)
+static char	*ft_set(char *str, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i != len)
+	{
+		str[i] = '0';
+		i++;
+	}
+	return (str);
+}
+
+static char	*ft_itoa(int n)
+{
+	char	*str;
+	size_t	len;
+	size_t	i;
+	long	nb;
+
+	nb = n;
+	len = ft_intlen(nb);
+	i = len;
+	str = malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
+	str = ft_set(str, len);
+	if (nb < 0)
+	{
+		nb *= -1;
+		str[0] = '-';
+	}
+	while (nb > 0)
+	{
+		str[len - 1] = nb % 10 + '0';
+		nb /= 10;
+		len--;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int	main(void)
 {
 	char	*str;
 
-	n += '0';
-	str = malloc(sizeof(char) * ft_strlen(n));
+	str = ft_itoa(-2147483648);
+	printf ("str = %s\n", str);
+	free(str);
 }
