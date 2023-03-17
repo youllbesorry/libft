009952ab54c371bf5382b5b0d063bae12a4bcf7a
youllbesorry <51507664+youllbesorry@users.noreply.github.com>
2023-03-17 22:02:33 +0000

    add ft_strfjoin

---
+++ Libft/src/ft_strfjoin.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfaure <bfaure@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 11:15:42 by bfaure            #+#    #+#             */
+/*   Updated: 2022/11/18 11:15:42 by bfaure           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/libft.h"
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		k;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!str)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		str[i] = s1[i];
+	k = 0;
+	while (s2[k])
+		str[i++] = s2[k++];
+	return (str[i] = '\0', str);
+}
